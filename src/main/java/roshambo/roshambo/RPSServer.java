6a59295777ba54f3756d038a07351161249b31c2
frankjlin16 <flin30@asu.edu>
2022-04-17 22:02:54 -0700

    Pre-populate server code

--- src/main/java/roshambo/roshambo/RPSServer.java
+++ src/main/java/roshambo/roshambo/RPSServer.java
@@ -1,4 +1,49 @@
 package roshambo.roshambo;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class RPSServer {
+    private int connections = 1;
+
+    public void start(Stage primaryStage) {
+
+        // Create scene and add to stage
+        Scene scene = new Scene(new BorderPane(), 400, 400);
+        primaryStage.setTitle("RPS Server");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    class HandleSession implements Runnable {
+
+        private Socket player1;
+        private Socket player2;
+        private DataInputStream in1;
+        private DataInputStream in2;
+        private DataOutputStream out1;
+        private DataOutputStream out2;
+
+        public HandleSession(Socket player1, Socket player2) {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            
+        }
+
+        private boolean hasWon() {
+            // TODO Auto-generated method stub
+            return false;
+        }
+        
+    }
 }
