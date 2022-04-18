c8cffba23f042e0b7904bcaef84b2b7b1fd844e5
frankjlin16 <flin30@asu.edu>
2022-04-17 23:59:32 -0700

    Update GUI

--- src/roshambo/RPSClient.java
+++ src/roshambo/RPSClient.java
@@ -3,6 +3,7 @@ package roshambo;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -12,8 +13,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class RPSClient extends Application {
@@ -21,6 +24,7 @@ public class RPSClient extends Application {
     // Core variables
     Choice myChoice;
     Choice opponentChoice;
+    String choices[] = { "Rock", "Paper", "Scissors" };
 
     // IO variables
     DataInputStream fromServer;
@@ -35,37 +39,34 @@ public class RPSClient extends Application {
 
         // Create border pane
         GridPane bottomGrid = new GridPane();
-        GridPane topGrid = new GridPane();
 
         // Create the bottom choice buttons
         for (int i = 0; i < 3; i++) {
+            StackPane stack = new StackPane();
+            Text text = new Text(choices[i]);
             Choice choice = new Choice(i);
-            GridPane.setMargin(choice, new Insets(10));
-            bottomGrid.add(choice, i, 1);
-        }
-
-        // Create the top choice buttons
-        for (int i = 0; i < 3; i++) {
-            Choice choice = new Choice(i);
-            GridPane.setMargin(choice, new Insets(10));
-            topGrid.add(choice, i, 0);
+            stack.getChildren().addAll(text, choice);
+            GridPane.setMargin(stack, new Insets(10));
+            bottomGrid.add(stack, i, 1);
         }
 
         // Align grids center
         bottomGrid.setAlignment(Pos.CENTER);
-        topGrid.setAlignment(Pos.CENTER);
 
         // Create border pane
         BorderPane border = new BorderPane();
         border.setBottom(bottomGrid);
-        border.setTop(topGrid);
         BorderPane.setMargin(bottomGrid, new Insets(10));
-        BorderPane.setMargin(topGrid, new Insets(10));
         BorderPane.setAlignment(bottomGrid, Pos.CENTER);
-        BorderPane.setAlignment(topGrid, Pos.CENTER);
+        
+        // Add oppenent rectangle
+        Rectangle opponent = new Rectangle(100, 60, Color.TRANSPARENT);
+        opponent.setStroke(Color.BLACK);
+        border.setTop(opponent);
+        BorderPane.setMargin(opponent, new Insets(20));
+        BorderPane.setAlignment(opponent, Pos.CENTER);
 
         // Create the message in the center
-        message.setText("Waiting for opponent...");
         border.setCenter(message);
 
         // Add button to side
@@ -78,10 +79,27 @@ public class RPSClient extends Application {
         stage.setTitle("RSP Client");
         stage.setScene(scene);
         stage.show();
+
+        // Connect to server
+        connectToServer();
     }
 
     private void connectToServer() {
+        try {
+            Socket socket = new Socket(host, 8000);
+            fromServer = new DataInputStream(socket.getInputStream());
+            toServer = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        // Start thread to listen for server messages
+        new Thread(() -> {
+            try {
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     private void sendChoice() {
@@ -104,7 +122,8 @@ public class RPSClient extends Application {
         private int choice;
 
         public Choice(int choice) {
-            super(100, 60, Color.RED);
+            super(100, 60, Color.TRANSPARENT);
+            this.setStroke(Color.BLACK);
             this.choice = choice;
             this.setOnMouseClicked(e -> handleMouseClicked());
         }
@@ -117,12 +136,20 @@ public class RPSClient extends Application {
             this.choice = choice;
         }
 
-        protected void outline() {
-            // TODO Auto-generated method stub
+        protected void outline(int x) {
+            if (x == 0) {
+                this.setStroke(Color.BLACK);
+            } else {
+                this.setStroke(Color.RED);
+            }
         }
 
         private void handleMouseClicked() {
-            // TODO Auto-generated method stub
+            if (myChoice != null) {
+                myChoice.outline(0);
+            }
+            myChoice = this;
+            outline(1);
         }
     }
 }
