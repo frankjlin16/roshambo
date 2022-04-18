8818d97f992cbe7b03e132cab7da2f0087ce8997
frankjlin16 <flin30@asu.edu>
2022-04-17 23:37:23 -0700

    Organize import

--- src/roshambo/RPSClient.java
+++ src/roshambo/RPSClient.java
@@ -3,7 +3,6 @@ package roshambo;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Timer;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -13,9 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
