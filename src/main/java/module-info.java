3ded2edc0b501da574aee8dfb037ea012802a0fe
frankjlin16 <flin30@asu.edu>
2022-04-17 22:42:44 -0700

    Update files

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module roshambo.roshambo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens roshambo.roshambo to javafx.fxml;
-    exports roshambo.roshambo;
-}
